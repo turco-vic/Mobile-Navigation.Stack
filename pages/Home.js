83cfa8145551fb5794818c1c4c42551518aaa17b
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-26 21:15:29 -0300

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -4,9 +4,7 @@ import Button from "../components/Button";
 export default function Home({ navigation }) {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Persistence and Navigation</Text>
-            <Text style={styles.textRed}>Navigation</Text>
-            <Text style={styles.textGreen}>Stack</Text>
+            <Text style={styles.title}>Navigation Stack</Text>
             <Button text="Detalhes" onPress={() => navigation.navigate("Detalhes")} />
         </View>
     );
@@ -22,24 +20,5 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 40,
         textAlign: "center",
-    },
-    input: {
-        height: 40,
-        width: 300,
-        borderColor: "gray",
-        borderWidth: 1,
-        marginTop: 30,
-        padding: 10,
-    },
-
-    textRed: {
-        color: "red",
-        marginTop: 20,
-        fontSize: 20,
-    },
-    textGreen: {
-        color: "green",
-        marginTop: 20,
-        fontSize: 20,
     },
 });
